33b8c89eea1de7e1c200e1a2feffbc6f586f36af
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-03-12 14:10:07 +0600

    loader added

--- src/Componenets/ui/Loader.js
+++ src/Componenets/ui/Loader.js
@@ -3,16 +3,18 @@ import {  RotatingLines } from "react-loader-spinner";
 
 function Loader() {
   return (
-    <div className='flex h-[60vh] self-center m-auto'>
+    <div className='h-[40vh] flex '>
    
-
-   <RotatingLines
-  strokeColor="grey"
-  strokeWidth="5"
-  animationDuration="0.75"
-  width="96"
-  visible={true}
-/>
+        <div className='self-center'>
+        <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+        />
+        </div>
+  
     </div>
   )
 }
