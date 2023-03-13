e91aab5cb750a9f4a9b0cdb3984759d2902f04e3
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-03-13 16:02:07 +0600

    tag with related video section done

--- src/Componenets/list/RelatedVideoListItem.js
+++ src/Componenets/list/RelatedVideoListItem.js
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
 export default function RelatedVideoListItem({ video }) {
-    const { id, thumbnail, title, duration, author, views, date } = video || {};
+    const { _id, thumbnail, title, duration, author, views, date } = video || {};
 
     return (
         <div class="w-full flex flex-row gap-2 mb-4">
             <div class="relative w-[168px] h-[94px] flex-none duration-300 hover:scale-[1.03]">
-                <Link to={`/videos/${id}`}>
+                <Link to={`/videos/${_id}`}>
                     <img src={thumbnail} class="object-cover" alt={title} />
                 </Link>
                 <p class="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
@@ -15,12 +15,12 @@ export default function RelatedVideoListItem({ video }) {
             </div>
 
             <div clas="flex flex-col w-full">
-                <Link to={`/videos/${id}`}>
+                <Link to={`/videos/${_id}`}>
                     <p class="text-slate-900 text-sm font-semibold">{title}</p>
                 </Link>
                 <Link
                     class="text-gray-400 text-xs mt-2 hover:text-gray-600"
-                    to={`/videos/${id}`}
+                    to={`/videos/${_id}`}
                 >
                     {author}
                 </Link>
